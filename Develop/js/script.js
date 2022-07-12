b384a1f38f420a9b047e183c1a22e932bb080031
Daniel Merkin <dan.merkin@gmail.com>
2022-07-12 11:51:17 -0400

    added variables with question objects with the answers

---
+++ Develop/js/script.js
@@ -0,0 +1,31 @@
+let score = 0;
+let questions = [
+  {
+    prompt: "What year is it?\n(a) 1990\n(b) 1873\n(c) 2022\n(d) 2222",
+    answer: "c"
+  },
+  {
+    prompt: "What year is it?\n(a) 1990\n(b) 1873\n(c) 2022\n(d) 2222",
+    answer: "c"
+  },
+  {
+    prompt: "What year is it?\n(a) 1990\n(b) 1873\n(c) 2022\n(d) 2222",
+    answer: "c"
+  },
+  {
+    prompt: "What year is it?\n(a) 1990\n(b) 1873\n(c) 2022\n(d) 2222",
+    answer: "c"
+  }
+]
+
+// for(var i = 0; i < questions.length; i++) {
+//   let response = prompt(questions[i].prompt);
+//   if (response == questions[i].answer) {
+//     score++;
+//     alert("correct")
+//   } else {
+//     alert("Nope")
+//   }
+// }
+
+// alert("you got " + score + "/" + questions.length)
