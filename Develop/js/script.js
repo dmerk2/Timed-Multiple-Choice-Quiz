c2eda6f6960db971c393b8ced0ca8e88d79875fb
Daniel Merkin <dan.merkin@gmail.com>
2022-07-12 13:58:56 -0400

    Created 4 code related Q and A

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,31 +1,31 @@
 let score = 0;
 let questions = [
   {
-    prompt: "What year is it?\n(a) 1990\n(b) 1873\n(c) 2022\n(d) 2222",
+    prompt: "What is an array?\n(a) An HTML element\n(b) A JavaScript function\n(c) A variable that can hold more than one value \n(d) A parameter of a function",
     answer: "c"
   },
   {
-    prompt: "What year is it?\n(a) 1990\n(b) 1873\n(c) 2022\n(d) 2222",
+    prompt: "What number does an array start?\n(a) 1\n(b) 2\n(c) 0\n(d) -1",
     answer: "c"
   },
   {
-    prompt: "What year is it?\n(a) 1990\n(b) 1873\n(c) 2022\n(d) 2222",
-    answer: "c"
+    prompt: "Is Java and JavaScript the same?\n(a) yes\n(b) no",
+    answer: "a"
   },
   {
-    prompt: "What year is it?\n(a) 1990\n(b) 1873\n(c) 2022\n(d) 2222",
-    answer: "c"
+    prompt: "What does var stand for?\n(a) Another name for the DOM\n(b) Variable\n(c) Version\n(d) eventListener",
+    answer: "b"
   }
 ]
 
-// for(var i = 0; i < questions.length; i++) {
-//   let response = prompt(questions[i].prompt);
-//   if (response == questions[i].answer) {
-//     score++;
-//     alert("correct")
-//   } else {
-//     alert("Nope")
-//   }
-// }
+for(var i = 0; i < questions.length; i++) {
+  let response = prompt(questions[i].prompt);
+  if (response == questions[i].answer) {
+    score++;
+    alert("correct")
+  } else {
+    alert("Nope")
+  }
+}
 
-// alert("you got " + score + "/" + questions.length)
+alert("you got " + score + "/" + questions.length)
