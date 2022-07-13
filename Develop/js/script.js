3ee7b72dc5019b6e88c22ede5d50d91c2076b5c6
Daniel Merkin <dan.merkin@gmail.com>
2022-07-13 14:57:02 -0400

    created countdown timer for 15 seconds

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -17,15 +17,71 @@ let questions = [
     answer: "b"
   }
 ]
+let timer = 15;
+let wins = 0;
+let losses = 0;
+const timerEl = document.getElementById('countdown');
+const questionElContainer = document.getElementById('question-container')
+const startButton = document.querySelector("#start-btn");
+ 
 
-for(var i = 0; i < questions.length; i++) {
-  let response = prompt(questions[i].prompt);
-  if (response == questions[i].answer) {
-    score++;
-    alert("correct")
-  } else {
-    alert("Nope")
+// for(var i = 0; i < questions.length; i++) {
+//   let response = prompt(questions[i].prompt);
+//   if (response == questions[i].answer) {
+//     score++;
+//   }
+// }
+
+// alert("you got " + score + "/" + questions.length)
+
+const startBtn = document.getElementById("start-btn");
+startBtn.addEventListener("click", startGame);
+
+function startGame() {
+  console.log("Started");
+  startBtn.classList.add('hide');
+  questionElContainer.classList.remove('hide');
+  timeLeft = 5;
+  countdown();
+  if (timeLeft === 0) {
+    clearInterval(timeInterval);
   }
+  // setNextQuestion();
+}
+// Timer that counts down from 5
+function countdown() {
+  var timeLeft = 15;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = timeLeft + ' seconds remaining';
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else if (timeLeft === 0) {
+      // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
+      // timerEl.textContent = timeLeft + ' second remaining';
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = '';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+      displayMessage();
+    }
+  }, 1000);
 }
 
-alert("you got " + score + "/" + questions.length)
+const nextBtn = document.getElementById("next-btn")
+nextBtn.addEventListener("click", nextBtn)
+
+function nextQuestion () {
+  console.log("Next")
+}
+
+function selectAnswer() {
+
+}
