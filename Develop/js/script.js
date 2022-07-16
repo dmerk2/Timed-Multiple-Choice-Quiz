9f49f9a4d6128036cc3d1c843f521efda77c3343
Daniel Merkin <dan.merkin@gmail.com>
2022-07-15 22:57:45 -0400

    changing multiple choice questions and amount of time given for quiz

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,154 +1,7 @@
-// let score = 0;
-// let wins = 0; 
-// const timerEl = document.getElementById('countdown');
-// const questionContainerElement = document.getElementById('question-container');
-// const nextButton = document.getElementById("next-btn");
-// const startBtn = document.getElementById("start-btn");
-// const nextBtn = document.getElementById("next-btn");
-// const questionElement = document.getElementById("question");
-// const answerButtonsElement = document.getElementById("answer-btns")
-// let questions = [
-//   {
-//     question: "What is an array?",
-//     answers: [
-//       {text: "An HTML element", correct: false},
-//       {text: "A JavaScript function", correct: false},
-//       {text: "A variable that can hold more than one value", correct: true},
-//       {text: "A parameter within a function", correct: false}
-//     ]
-//   },
-//   {
-//     question: "What number does an array start?",
-//     answers: [
-//       {text: "1", correct: false},
-//       {text: "0", correct: true},
-//       {text: "2", correct: false},
-//       {text: "-1", correct: false}
-//     ]
-//   },
-//   {
-//     question: "Is Java and JavaScript the same?",
-//     answers: [
-//       {text: "yes", correct: false},
-//       {text: "no", correct: true}
-//     ]
-//   },
-//   {
-//     question: "What does var stand for?",
-//     answers: [
-//       {text: "Another Name for the DOM", correct: false},
-//       {text: "Variable", correct: true},
-//       {text: "Version", correct: false},
-//       {text: "eventListener", correct: false}
-//     ]
-//   }
-// ]
-// let shuffledQuestions, currentQuestionIndex;
-
-// startBtn.addEventListener("click", startGame);
-
-// function startGame() {
-//   startBtn.classList.add('hide');
-//   shuffledQuestions = questions.sort(() => Math.random() - .5)
-//   currentQuestionIndex = 0;
-//   questionContainerElement.classList.remove('hide');
-//   countdown();
-//   // setNextQuestion()
-// }
-// // Timer that counts down from 5
-// function countdown() {
-//   var timeLeft = 5;
-
-//   // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-//   var timeInterval = setInterval(function () {
-//     // As long as the `timeLeft` is greater than 1
-//     if (timeLeft > 1) {
-//       // Set the `textContent` of `timerEl` to show the remaining seconds
-//       timerEl.textContent = 'You have ' + timeLeft + ' seconds remaining';
-//       // Decrement `timeLeft` by 1
-//       timeLeft--;
-//     } else if (timeLeft === 0) {
-//       // When `timeLeft` is equal to 1, rename to 'second' instead of 'seconds'
-//       timerEl.textContent = 'You have' + timeLeft + ' second remaining';
-//       timeLeft--;
-//     } else {
-//       // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-//       timerEl.textContent = 'You are out of time!';
-//       // Use `clearInterval()` to stop the timer
-//       clearInterval(timeInterval);
-//     }
-//   }, 1000);
-// }
-
-// nextButton.addEventListener("click", function () {
-//   console.log("next")
-// })
-
-// // var questionBox = document.querySelector(".card");
-// // console.log
-// // for (let i = 0; i < questions.length; i++) {
-// //   const question = questions[i];
-// //   var questionNode = document.createElement("p");
-// //   questionNode.appendChild(questionNode);
-// // };
-
-// function setNextQuestion() {
-//   showQuestion(shuffledQuestions[currentQuestionIndex])
-// }
-
-// function showQuestion (question) {
-//   questionElement.innerText = question.question
-//   question.answers.forEach(function answer() {
-//     const button = document.createElement('button')
-//     button.innerText = answer.text
-//     button.classList.add("btn")
-//     if (answer.correct) {
-//       button.dataset.correct = answer.correct
-//     }
-//     button.addEventListener("click", selectAnswer)
-//     answerButtonsElement.appendChild(button)
-//   });
-// }
-
-// function selectAnswer(e) {
-//   const selectedButton = e.target
-//   const correct = selectedButton.dataset.correct
-//   setStatusClass(document.body, correct)
-//   Array.from(answerButtonsElement.children).forEach(button => {
-//     setStatusClass(button, button.dataset.correct)
-//   })
-//   nextButton.classList.remove('hide')
-// }
-
-// function setStatusClass(element, correct) {
-//   clearStatusClass(element)
-//   if (correct) {
-//     element.classList.add('correct')
-//   } else {
-//     element.classList.add('wrong')
-//   }
-// }
-
-// function clearStatusClass(element) {
-//   element.classList.remove('correct')
-//   element.classList.remove('wrong')
-// }
-
-// // function winGame() {
-// //   wins++
-// // }
-
-// // for(var i = 0; i < questions.length; i++) {
-// //   let response = console.log(questions[i]);
-// //   if (response == questions[i].answer) {
-// //     wins++;
-// //   }
-// // }
-
 //Questions, options and answers are stored in an array
 var questions = [
   {
-    questionText: "Inside which HTML element do we put the javascript?",
+    questionText: "Where does your JavaScript get embedded inside of your HTML file?",
     options: [
       "1. <h1>",
       "2. <js>",
@@ -157,7 +10,7 @@ var questions = [
     answer: "3. <script>",
   },
   {
-    questionText: "What defines the logical structure of documents and the way a document is accessed and manipulated?",
+    questionText: "What does the DOM stand for?",
     options: [
       "1. do overnight modules",
       "2. document object model",
@@ -166,7 +19,7 @@ var questions = [
     answer: "2. document object model",
   },
   {
-    questionText: "String values must be enclosed within _____ when being assigned to variables.",
+    questionText: "String values must be enclosed in what?",
     options: [
       "1. commas",
       "2. curly brackets",
